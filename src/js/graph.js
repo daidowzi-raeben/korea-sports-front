09c183d857d11859c23e153c02cdb561e0834eca
daidowzi-raeben <raeben@naver.com>
2021-11-23 17:22:40 +0900

    graph fix

--- src/js/graph.js
+++ src/js/graph.js
@@ -21,26 +21,34 @@ $(document).ready(function () {
     $('.container-graph > div.item > span').css({'height': pH + 'px'});
 
     barTotal = [];
+    barMax = [];
+    // var barMax = new Array();
     var _barDataLength = _elBar.children('.container-graph').eq(0).find('div.item').length;
     _elBar.children('.container-graph').each(function(key, val) {
-        barTotal[key] = 0
+        barTotal[key] = 0;
+        barMax[key] = new Array();
+        // barMax[key] = new Array();
         _elBar.children('.container-graph').eq(key).find('div.item').each(function(key2, val2) {
             _barDataOption = _elBar.children('.container-graph').eq(key).find('div.item').eq(key2).data('option');
-            barTotal[key] += Number(_barDataOption)
+            barTotal[key] += Number(_barDataOption);
+            barMax[key].push(_barDataOption);
         })
-
+        totalLengthItem = _elBar.children('.container-graph').eq(key).find('div.item').length;
+        __max = Math.max.apply(null, barMax[key]);
         _elBar.children('.container-graph').eq(key).find('div.item').each(function(key2, val2) {
             _elHeight = _elBar.children('.container-graph').eq(key).find('div.item').eq(key2).data('option');
             _elDataTitle= _elBar.children('.container-graph').eq(key).find('div.item').eq(key2).data('title');
             _totalHeight = _elBar.children('.container-graph').height();
-            var j = (pH / 100) * (_elHeight / barTotal[key]) * 100;
-                j < 3 ? j = 5 : j = j;
-                j > 100 ? j = "calc(100% - 40px)" : j = j + '%';
+            var j = _elHeight / __max  * 100;           
+            // console.log((barTotal[key] / totalLengthItem));
+            // console.log(j);
+                j < 20 ? j = 20 : j = j;
+                j == 100 ? j = "calc(100% - 40px)" : j = "calc("+j+"% - 40px)";
             _elBar.children('.container-graph').eq(key).find('div.item').children('span').eq(key2).css({'height' :  j });
             _append = '<div class="data-option">'+ __comma(_elHeight) +'</div>';
             _appendTitle = '<div class="data-title">'+_elDataTitle+'</div>';
             _elBar.children('.container-graph').eq(key).find('div.item').eq(key2).append(_append + _appendTitle);
-            $('.container-graph').eq(key).children('div.item').find('.data-option').eq(key2).css({'bottom' : j})
+            $('.container-graph').eq(key).children('div.item').find('.data-option').eq(key2).css({'bottom' : j })
         })
 
     })
@@ -55,7 +63,7 @@ $(document).ready(function () {
                 jQuery(".graph-scroll--bar ul.container").css("left", __w / 3 + 'px');
         }
     });
-    
+
    
 });
 
